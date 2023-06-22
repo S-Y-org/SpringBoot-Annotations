9198de417c2dd8be292570e044f6c1f2e471a051
IT21440236 <it21440236@my.sliit.lk>
2023-06-22 21:51:24 +0530

    commit 01

---
+++ src/test/java/com/spring/springannotations/SpringAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springannotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnnotationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
